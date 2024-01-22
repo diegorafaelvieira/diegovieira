1ee3502d1ec4351e119db38baa38a44e585422da
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2024-01-21 23:18:09 -0300

    Add Projeto Chrono

---
+++ projetos/chrono-trigger/README.md
@@ -0,0 +1,6 @@
+# chrono-trigger
+ Site Chrono Trigger com conhecimentos até o momento do Curso de WebDesign da Origamid.
+
+[SITE](https://diegorafaelvieira.github.io/chrono-trigger/) :desktop_computer: 
+
+
---
+++ projetos/chrono-trigger/js/script.js
@@ -0,0 +1,20 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"
+        time: 5000 // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: "screen", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true // se deve ou não mostrar a navegação
+    });
+
+}
+
+if(window.SimpleAnime) {
+
+    new SimpleAnime();
+    
+}
